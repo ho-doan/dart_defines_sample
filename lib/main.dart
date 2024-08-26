cc8369a850d0a028d9abe2cfe1b56a6fc59dadb5
HoDoanOmii <hodp@ominext.com>
2024-08-26 15:30:12 +0700

    move class env dart to enum

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(
-              Env.instance.appName,
+              Env.appFlavor.value,
             ),
           ],
         ),
--- lib/src/core/env/env.dart
+++ lib/src/core/env/env.dart
@@ -1,10 +1,11 @@
-class Env {
-  const Env._();
+enum Env {
+  appName(String.fromEnvironment('APP_NAME')),
+  appFlavor(String.fromEnvironment('APP_FLAVOR')),
+  appWebLink(String.fromEnvironment('APP_WEB_LINK')),
+  appSuffix(String.fromEnvironment('APP_ID_SUFFIX'));
 
-  static const instance = Env._();
-
-  String get appName => const String.fromEnvironment('APP_NAME');
-  String get appFlavor => const String.fromEnvironment('APP_FLAVOR');
-  String get appWebLink => const String.fromEnvironment('APP_WEB_LINK');
-  String get appSuffix => const String.fromEnvironment('APP_ID_SUFFIX');
+  const Env(this.value);
+  final String value;
 }
+
+const env = [];
