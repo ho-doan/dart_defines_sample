86924c9030c2fd85f51a0f41f513aa464c49cc18
HoDoanOmii <hodp@ominext.com>
2024-08-26 11:48:57 +0700

    ini

---
+++ ios/Scripts/decode_dart_defines.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function decode() { echo "${*}" | base64 --decode; }
+
+export_file="${SRCROOT}/Flutter/DartDefines.xcconfig"
+
+IFS=',' read -r -a dart_defines <<< "$DART_DEFINES"
+
+: > "${SRCROOT}/Flutter/DartDefines.xcconfig"
+
+for index in "${!dart_defines[@]}"
+do
+  dart_define=$(decode "${dart_defines[$index]}")
+  IFS='=' read -r -a key_value <<< "$dart_define"
+  export ${key_value[0]}=${key_value[1]}
+  echo "${key_value[0]}=${key_value[1]}" >> ${export_file}
+done
+
+# cp "$PROJECT_DIR/firebase_app_id_file_${APP_FLAVOR}.json" "$PROJECT_DIR/firebase_app_id_file.json"
+# cp "$PROJECT_DIR/GoogleServices/GoogleService-Info_${APP_FLAVOR}.plist" "$PROJECT_DIR/Runner/GoogleService-Info.plist"
