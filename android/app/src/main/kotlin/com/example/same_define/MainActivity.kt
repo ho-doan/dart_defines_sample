d5d64b10ba29e39a972d9eb0b4f4eaca9ccad501
HoDoanOmii <hodp@ominext.com>
2024-08-26 13:50:21 +0700

    complete-android

--- android/app/src/main/kotlin/com/example/same_define/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/same_define/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.same_define
 
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("///", "onCreate: ${getString(R.string.appName)}")
+    }
+}
